327390da8d001221e90e780761d403c2f829b2f0
Robert Schönthal <caziel@gmx.net>
2011-02-15 03:31:09 +0100

    =formatting

--- Command/bootstrapCommand.php
+++ Command/bootstrapCommand.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
 use Symfony\Bundle\FrameworkBundle\Util\Mustache;
-
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 
 /**
@@ -19,19 +18,20 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
  */
 class BootstrapCommand extends Command
 {
+
 	private $defaultConfig = 'project_bootstrap.yml';
-	
-    /**
-     * @see Command
-     */
-    protected function configure()
-    {		
-        $this
-            ->setDefinition(array(
-				new InputOption('config', 'c', InputOption::VALUE_OPTIONAL, 'config file to use',$this->defaultConfig),
-				new InputOption('stop', null, InputOption::VALUE_NONE, 'stop on error')
-            ))
-            ->setHelp(<<<EOT
+
+	/**
+	 * @see Command
+	 */
+	protected function configure()
+	{
+		$this
+				->setDefinition(array(
+					new InputOption('config', 'c', InputOption::VALUE_OPTIONAL, 'config file to use', $this->defaultConfig),
+					new InputOption('stop', null, InputOption::VALUE_NONE, 'stop on error')
+				))
+				->setHelp(<<<EOT
 The <info>project:bootstrap</info> command bootstraps your application with symfony commands.
 
 <info>./app/console project:bootstrap --config=~/foo.yml</info> loads a custom config
@@ -47,27 +47,27 @@ shells:
 </comment>
 <info>./app/console project:bootstrap --stop</info> may raise an exception
 EOT
-            )
-            ->setName('project:bootstrap')
-        ;
-    }
+				)
+				->setName('project:bootstrap')
+		;
+	}
 
-    /**
-     * @see Command
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+	/**
+	 * @see Command
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
 		$this->output = $output;
 		$this->stopOnError = $input->getOption('stop');
 		$config = $this->loadConfigFile($input->getOption('config'));
-		
+
 		$this->setApplication(new Application($this->container->get('kernel')));
 		$this->application->setCatchExceptions(!$this->stopOnError);
 		$this->application->setAutoExit(false);
 
 		$this->doCommands($config['commands']);
-    }
-	
+	}
+
 	/**
 	 * loads the config file
 	 * 
@@ -77,20 +77,18 @@ EOT
 	protected function loadConfigFile($file)
 	{
 		//default
-		if($file === $this->defaultConfig)
-		{
+		if ($file === $this->defaultConfig) {
 			$kernel = $this->container->get('kernel');
-			$file = $kernel->getRootDir().\DIRECTORY_SEPARATOR.'config'.\DIRECTORY_SEPARATOR.$file;
+			$file = $kernel->getRootDir() . \DIRECTORY_SEPARATOR . 'config' . \DIRECTORY_SEPARATOR . $file;
 		}
-		
-		if(!\is_file($file))
-		{
-			throw new \InvalidArgumentException(sprintf('configuration file not found [%s]',$file));
+
+		if (!\is_file($file)) {
+			throw new \InvalidArgumentException(sprintf('configuration file not found [%s]', $file));
 		}
-		
+
 		return \Symfony\Component\Yaml\Yaml::load($file);
 	}
-	
+
 	/**
 	 * run commands
 	 * 
@@ -99,13 +97,13 @@ EOT
 	protected function doCommands($commands)
 	{
 		//supress warnings if stop on error
-		if($this->stopOnError){
-			@\array_map(array($this,'processCommand'), $commands);
-		}else{
-			\array_map(array($this,'processCommand'), $commands);
+		if ($this->stopOnError) {
+			@\array_map(array($this, 'processCommand'), $commands);
+		} else {
+			\array_map(array($this, 'processCommand'), $commands);
 		}
 	}
-	
+
 	/**
 	 * runs a command
 	 * 
@@ -117,10 +115,10 @@ EOT
 		$this->output->writeln(sprintf('<question>execute</question> <comment>%s</comment>', $command));
 
 		//create a input from command line
-		$input = strpos($command,' ') !== false ? new \Symfony\Component\Console\Input\StringInput($command) : new \Symfony\Component\Console\Input\ArrayInput(array($command));
-	
+		$input = strpos($command, ' ') !== false ? new \Symfony\Component\Console\Input\StringInput($command) : new \Symfony\Component\Console\Input\ArrayInput(array($command));
+
 		//run the command
 		return $this->application->run($input);
 	}
-	
+
 }
