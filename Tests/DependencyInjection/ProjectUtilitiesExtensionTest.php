ecb0bc2ae75d16d8057d1227b4076d06715037d6
Robert Schönthal <caziel@gmx.net>
2011-02-18 02:17:50 +0100

    fixed DI

---
+++ Tests/DependencyInjection/ProjectUtilitiesExtensionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace rs\ProjectUtilitiesBundle\Tests\DependencyInjection;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Console\Tester\ApplicationTester;
+use Symfony\Component\Console\Output\NullOutput;
+
+use rs\ProjectUtilitiesBundle\Tests\TestCase as BaseTestCase;
+
+
+class ProjectUtilitiesExtensionTest extends BaseTestCase
+{
+	
+	
+    public function testGetService()
+    {
+		$this->markTestIncomplete();
+		$service = $this->getKernel()->getContainer()->get('project_utilities');
+		var_dump($service);
+	}	
+		
+    protected function tearDown()
+    {
+    }	
+	
+}
